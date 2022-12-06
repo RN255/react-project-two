1aec3434483e52fe38eb91f33027cdcb4ff739ed
Robert Nickson <106345947+RN255@users.noreply.github.com>
2022-12-06 15:58:21 +0000

    all complete round one

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,56 @@
 import { Link } from "react-router-dom";
 import planelogo from "./images/airplane-mode.png";
+import menuIcon from "./images/menu.png";
+import closeMenuIcon from "./images/cross.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  function openMobileMenu() {
+    setShowMenu(true);
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeMobileMenu() {
+    setShowMenu(false);
+    document.body.style.overflow = "unset";
+  }
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  let menu;
+
+  if (showMenu) {
+    menu = (
+      <nav id="Mobilenav">
+        <img
+          src={closeMenuIcon}
+          alt="close menu icon"
+          id="closeMenuIcon"
+          onClick={closeMobileMenu}
+        ></img>
+        <h1>Superflight</h1>
+        <ul>
+          <li>
+            <Link to="/" onClick={closeMobileMenu}>
+              Search flights
+            </Link>
+          </li>
+          <li>
+            <Link to="/Tips" onClick={closeMobileMenu}>
+              Search tips
+            </Link>
+          </li>
+          <li>
+            <Link to="/Partnership" onClick={closeMobileMenu}>
+              Partnership
+            </Link>
+          </li>
+          {/* <Link to="/journeysList">Journey List</Link> */}
+        </ul>
+      </nav>
+    );
+  }
+
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
@@ -20,6 +69,13 @@ export default function Navbar() {
         </li>
         {/* <Link to="/journeysList">Journey List</Link> */}
       </ul>
+      <img
+        src={menuIcon}
+        alt="mobile menu icon"
+        id="menuIcon"
+        onClick={openMobileMenu}
+      ></img>
+      {menu}
     </nav>
   );
 }
