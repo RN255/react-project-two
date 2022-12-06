175424d6ac466e6c0cf1d4e3a9f9d14cbf8a2f08
Robert Nickson <106345947+RN255@users.noreply.github.com>
2022-12-06 13:45:05 +0000

    form and current journeys mobile view done

--- src/pages/Journey.js
+++ src/pages/Journey.js
@@ -6,10 +6,15 @@ export default function Journey({ journeys, deleteJourney }) {
   return (
     <>
       <div id="journeyInfoContainer">
+        <p className="mobileScreenJourneyInfo">From:</p>
         <p id="journeyFrom">{journeys.from}</p>
+        <p className="mobileScreenJourneyInfo">To:</p>
         <p>{journeys.to}</p>
+        <p className="mobileScreenJourneyInfo">Depart:</p>
         <p>{journeys.departDate}</p>
+        <p className="mobileScreenJourneyInfo">Arrive:</p>
         <p>{journeys.returnDate}</p>
+        <p className="mobileScreenJourneyInfo">Cabin class:</p>
         <p>{journeys.cabinClass}</p>
         <button onClick={handleClick}>delete</button>
       </div>
--- src/pages/JourneysList.js
+++ src/pages/JourneysList.js
@@ -1,30 +1,36 @@
 import Journey from "./Journey";
 
-export default function JourneysList({ journeys, deleteJourney, noJourneysMsg }) {
+export default function JourneysList({
+  journeys,
+  deleteJourney,
+  noJourneysMsg,
+}) {
   return (
     <>
       <div id="JourneyMainHolder">
         <h2>Current journeys</h2>
-        <div id="journeyTitles">
-          <p>From:</p>
-          <p>To:</p>
-          <p>Depature date:</p>
-          <p>Return date:</p>
-          <p>Cabin class:</p>
-          <div></div>
+        <div id="JourneyInnerHolder">
+          <div id="journeyTitles">
+            <p>From:</p>
+            <p>To:</p>
+            <p>Depature date:</p>
+            <p>Return date:</p>
+            <p>Cabin class:</p>
+            <div></div>
+          </div>
+          <p id="noJourneyMsg">{noJourneysMsg}</p>
+          {journeys.map((journeys) => {
+            return (
+              <>
+                <Journey
+                  key={journeys.id}
+                  journeys={journeys}
+                  deleteJourney={deleteJourney}
+                />
+              </>
+            );
+          })}
         </div>
-        <p id="noJourneyMsg">{noJourneysMsg}</p>
-        {journeys.map((journeys) => {
-          return (
-            <>
-              <Journey
-                key={journeys.id}
-                journeys={journeys}
-                deleteJourney={deleteJourney}
-              />
-            </>
-          );
-        })}
       </div>
     </>
   );
